ddf10d321010ed81f1a86df25a13bb32a67f686c
Eleonore <el.pc.wald@gmail.com>
2019-06-25 15:03:01 -0400

    Bumper Ok

---
+++ Assets/Scripts/BumperManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumperManager : MonoBehaviour
+{
+    [SerializeField] public float magnitude = 500;
+    public void OnCollisionEnter(Collision other)
+    {
+        // how much the character should be knocked back
+       
+        // calculate force vector
+        var force = other.transform.position - transform.position;
+        // normalize force vector to get direction only and trim magnitude
+        force.Normalize();
+        other.rigidbody.AddForce(force * magnitude);
+    }
+}
