0dc52fe0d3aefaff4f94c7867b3e915eeba76f69
Eleonore <el.pc.wald@gmail.com>
2019-06-25 16:53:25 -0400

    Ball Change Ok

---
+++ Assets/Scripts/Ball/Ball.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Ball : MonoBehaviour
+{
+    public Vector3 Velocity { get { return rb.velocity; }
+        set
+        {
+            if (rb == null)
+                rb = GetComponent<Rigidbody>();
+            rb.velocity = value;
+        } }
+    [SerializeField]
+    Stats stats;
+    
+    // ---- INTERN ----
+    private Vector3 movement = Vector3.zero;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        rb.AddForce(movement * stats.speed);
+    }
+
+    public void Move(Vector3 movement)
+    {
+        this.movement = movement;
+    }
+}
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] List<GameObject> balls = new List<GameObject>();
+    [SerializeField] int currentBallIndex = 0;
+    [SerializeField] Transform ballContainer = default;
+    Ball currentBall;
+
+    private void Start()
     {
-        
+        currentBallIndex = 0;
+        currentBall = ballContainer.transform.GetChild(0).gameObject.GetComponent<Ball>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("ChangeBall"))
+        {
+            currentBallIndex = (currentBallIndex == balls.Count - 1) ? 0 : currentBallIndex+1;
+            GameObject ballClone = (GameObject) Instantiate(balls[currentBallIndex], currentBall.transform.position, currentBall.transform.rotation, ballContainer);
+            Vector3 velocity = currentBall.Velocity;
+            Destroy(currentBall.gameObject);
+            currentBall = ballClone.GetComponent<Ball>();
+            currentBall.Velocity = velocity;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        currentBall.Move(movement);
     }
 }
